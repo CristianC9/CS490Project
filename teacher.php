d7099ba116c69581328ae57608f6b9b6e79a3881
Juan Lopez <54092513+jlopz135@users.noreply.github.com>
2022-09-25 19:45:28 -0400

    Teacher Alpha

---
+++ teacher.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Teacher Page
+        </title>
+    </head>
+    <body>
+        <h1>Welcome to the Teacher Page</h1>
+    </body>
+</html>
