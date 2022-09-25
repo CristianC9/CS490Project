9b4e0845e5f1e03abd6e3f5c31f0c354fb4abc80
Juan Lopez <54092513+jlopz135@users.noreply.github.com>
2022-09-25 19:44:56 -0400

    Student Alpha

---
+++ student.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Student Page
+        </title>
+    </head>
+    <body>
+        <h1>Welcome to the Student Page</h1>
+    </body>
+</html>
