4047e9733d738cb4646f8db0bbc9e0fc3aef7ba6
Juan Lopez <54092513+jlopz135@users.noreply.github.com>
2022-12-12 17:19:31 -0500

    Final Version

--- teacher/editQuestion.php
+++ teacher/editQuestion.php
@@ -14,13 +14,22 @@
         }
     }
     
+    //echo $_POST['constraint'];
+
 }
         
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 
 <!DOCTYPE html>
 <html>
+<style>
+    body{
+        padding: 20px;
+        background: #EEEEEE;
+    }
+</style>
     <head>
         <title>
             Edit Question
@@ -36,19 +45,19 @@
     ?>
     
     <h1>Edit Question Page</h1>
-    <a href="https://afsaccess4.njit.edu/~jl2237/teacher/teacher.php">HOME</a>
+    <a href="https://afsaccess4.njit.edu/~jl2237/teacher/teacher.php" class="btn btn-primary">HOME</a>
         <form method="POST" action="https://afsaccess4.njit.edu/~jl2237/backend/edit_backend.php"> 
             
             <h3>Function Name</h3>
-            <textarea id="functionname" name="functionname" rows="2" cols="25" placeholder="Function Name"><?php echo $_POST['function_name'] ?></textarea>
+            <textarea class="form-control-sm mb-2" id="functionname" name="functionname" rows="2" cols="25" placeholder="Function Name"><?php echo $_POST['function_name'] ?></textarea>
             <h3><label for="question">Insert Question:</label></h3>
 
                 <div>
-                    <textarea id="question" name="question" rows="4" cols="50" ><?php echo $_POST['question'] ?></textarea>
+                    <textarea class="form-control-sm mb-2" id="question" name="question" rows="4" cols="50" ><?php echo $_POST['question'] ?></textarea>
                 </div>
                 <h3>Question Topic</h3>
                 <br>
-                <select name="topic" id="topic">
+                <select class="form-select-sm mb-1" name="topic" id="topic">
                     <option value="<?php echo $_POST['topic'] ?>" selected><?php echo $_POST['topic'] ?></option>
                     <option value="variables">Variables</option>
                     <option value="forLoops">For Loops</option>
@@ -57,41 +66,36 @@
                     <option value="lists">Lists</option>
                 </select>
 
-                <select name="difficulty" id="difficulty">
+                <select class="form-select-sm mb-1" name="difficulty" id="difficulty">
                     <option value="<?php echo $_POST['difficulty'] ?>" selected><?php echo $_POST['difficulty'] ?></option>
                     <option value=1>Easy    (1)</option>
                     <option value=2>Medium  (2)</option>
                     <option value=3>Hard    (3)</option>
                     
                 </select>
+                
+                <select class="form-select-sm mb-1" name="constraint" id="constraint">
+                    <option value="<?php echo $_POST['constraint'] ?>" selected><?php echo $_POST['constraint'] ?></option>
+                    <option value="forLoops">For Loops</option>
+                    <option value="whileLoops">While Loops</option>
+                    <option value="recursion">Recursion</option>
+                </select>
+                
+                
                 <br><br>
                 <h3>Test Cases</h3>
-                <div>
-                    <textarea id="case1" name="case1" rows="2" cols="25" placeholder="Test Case 1"><?php echo $_POST['case1'] ?></textarea>
-                    <textarea id="output1" name="output1" rows="2" cols="25" placeholder="Expected Output 1"><?php echo $_POST['output1'] ?></textarea>
-                </div>
                 
-               <div>
-                    <textarea id="case2" name="case2" rows="2" cols="25" placeholder="Test Case 2"><?php echo $_POST['case2'] ?></textarea>
-                    <textarea id="output2" name="output2" rows="2" cols="25" placeholder="Expected Output 2"><?php echo $_POST['output2'] ?></textarea>
-                </div>
-                <!-- <div>
-                    <textarea id="case3" name="case3" rows="2" cols="25" placeholder="Test Case 3"></textarea>
-                    <textarea id="output3" name="output3" rows="2" cols="25" placeholder="Expected Output 3"></textarea>
-                </div>
-                <div>                    
-                    <textarea id="case4" name="case4" rows="2" cols="25" placeholder="Test Case 4"></textarea>
-                    <textarea id="output4" name="output4" rows="2" cols="25" placeholder="Expected Output 4"></textarea>
-                </div>
+                <?php for($i = 0; $i < count($_POST['case']); $i++){
+                    ?>
                 <div>
-                    <textarea id="case5" name="case5" rows="2" cols="25" placeholder="Test Case 5"></textarea>
-                    <textarea id="output5" name="output5" rows="2" cols="25" placeholder="Expected Output 5"></textarea>                </div> -->
+                    <input type="hidden" name="tcid[]" value="<?php echo $_POST['tcid'][$i]?>">
+                    <textarea id="case" class="form-control-sm mb-2" name="case[]" rows="2" cols="25" placeholder="Test Case 1"><?php echo $_POST['case'][$i] ?></textarea>
+                    <textarea id="output" class="form-control-sm mb-2" name="output[]" rows="2" cols="25" placeholder="Expected Output 1"><?php echo $_POST['output'][$i] ?></textarea>
+                </div>
+                <?php }?>
 
-                <input type="hidden" name="tcid1" value="<?php echo $_POST['tcid1']?>">
-                <input type="hidden" name="tcid2" value="<?php echo $_POST['tcid2']?>">
                 <input type="hidden" name="qid" value="<?php echo $_POST['qid']?>">
-
-                <input type="submit" class="mt-3 btn btn-dark" value="SUBMIT" />
+                <input type="submit" class="btn btn-primary" value="SUBMIT" />
         </form>
 
         <br><br><br><br>
