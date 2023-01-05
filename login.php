5111fa9e280846ea389d6ee6d162b374656f350a
Juan Lopez <54092513+jlopz135@users.noreply.github.com>
2023-01-05 17:26:05 -0500

    correct login

--- login.php
+++ login.php
@@ -1,43 +1,134 @@
-<?php
+<?php 
+session_start();
+require(__DIR__ . "/functions.php");
 
-function roles($response){
-
-    if($response == 'teacher'){
-
-        return "teacher";
-    }if($response == 'student'){
-        return "student";
-    }else{
-        return false;
-    }
-}
-function is_logged_in(){
-    $loggedIn = isset($_SESSION['role']);
-    if($loggedIn === true){
-        return true;
-    }else{
-        $_SESSION['error']= "Invalid Access";
-        die(header('Location: https://afsaccess4.njit.edu/~jl2237/login.php'));    
-        return false;
-    }
-}
-function displayMessage(){
-    if(isset($_SESSION['message'])){
-        echo $_SESSION['message'];
-        unset($_SESSION['message']);
-    }
-}
-function checkSet(){
-    if(isset($_POST["question"]) && isset($_POST["case"]) && isset($_POST["output"]) 
-    && isset($_POST["functionname"])){
-        return TRUE;
-    }
-    return FALSE;
-}
-function checkExam(){
-    if(isset($_POST["qid1"]) && isset($_POST["qid2"]) && isset($_POST["qid3"]) && isset($_POST["examName"]) && isset($_POST["description"])){
-        return TRUE;
-    }
-    return FALSE;
-}
 ?>
+<html>
+
+<head>
+    <title>
+        Login Page
+    </title>
+</head>
+
+    <div class="login-page">
+        <div class="form">
+        
+        <h1>Login</h1>
+            <?php
+                if(isset($_SESSION["error"])){
+                    $error = $_SESSION["error"];
+                    echo "<span>$error</span><br><br>";
+                }
+            ?> 
+        <form class="login-form" method="POST" action="https://afsaccess4.njit.edu/~jl2237/backend/frontback.php">
+                
+                <input type="text" id="email" name="email" placeholder="Username" required />
+                
+                <input type="password" id="password" name="password" placeholder="Password" required minlength="8" />
+        <br>
+            <button type="submit" value="Login">Login</button>
+            
+        </form>
+        </div>
+    </div>
+
+
+</html>
+<?php
+    unset($_SESSION["error"]);
+    
+?>
+
+<!-- stylying -->
+
+<style>
+
+    .login-page {
+    width: 360px;
+    padding: 8% 0 0;
+    margin: auto;
+    }
+    .form {
+    position: relative;
+    z-index: 1;
+    background: #FFFFFF;
+    max-width: 360px;
+    margin: 0 auto 100px;
+    padding: 45px;
+    text-align: center;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+    .form input {
+    outline: 0;
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+    }
+    .form button {
+    text-transform: uppercase;
+    outline: 0;
+    background: lightblue;
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    color: #FFFFFF;
+    font-size: 14px;
+    -webkit-transition: all 0.3 ease;
+    transition: all 0.3 ease;
+    cursor: pointer;
+    }
+    .form button:hover,.form button:active,.form button:focus {
+    background: lightgreen;
+    }
+    .form .message {
+    margin: 15px 0 0;
+    color: #b3b3b3;
+    font-size: 12px;
+    }
+    .form .register-form {
+    display: none;
+    }
+    .container {
+    position: relative;
+    z-index: 1;
+    max-width: 300px;
+    margin: 0 auto;
+    }
+    .container:before, .container:after {
+    content: "";
+    display: block;
+    clear: both;
+    }
+    .container .info {
+    margin: 50px auto;
+    text-align: center;
+    }
+    .container .info h1 {
+    margin: 0 0 15px;
+    padding: 0;
+    font-size: 36px;
+    font-weight: 300;
+    color: #1a1a1a;
+    }
+    .container .info span {
+    color: red;
+    font-size: 12px;
+    }
+    .container .info span a {
+    color: #000000;
+    text-decoration: none;
+    }
+    .container .info span .fa {
+    color: #EF3B3A;
+    }
+    body {
+    background: #EEEEEE;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;      
+    }
+</style>
